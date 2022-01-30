b0e880bceb6ee5b1b6b06dfb209b13971cac7f10
Takucap10 <t.okusako10@gmail.com>
2022-01-30 18:48:47 +0900

    view部分を作成

--- db/migrate/20220129081905_create_rooms.rb
+++ db/migrate/20220129081905_create_rooms.rb
@@ -1,7 +1,8 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
-
+      t.string :item, null: false
+      t.text :info,   null: false
       t.timestamps
     end
   end
--- db/migrate/20220129081945_create_memos.rb
+++ db/migrate/20220129081945_create_memos.rb
@@ -1,7 +1,8 @@
 class CreateMemos < ActiveRecord::Migration[6.0]
   def change
     create_table :memos do |t|
-
+      t.string :item, null: false
+      t.text :info,   null: false
       t.timestamps
     end
   end
