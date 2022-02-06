cadb12d26df45989e4ccc48a759b5fef3130d49f
Takucap10 <t.okusako10@gmail.com>
2022-02-06 14:21:16 +0900

    ログイン画面作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configue_parmitted_parameter, if: :devise_controller?
+
+  private
+  def configue_parmitted_parameter
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end
