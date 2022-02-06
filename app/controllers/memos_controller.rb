492b65bddb86e3c446e6f66d6723ae5adc66f6be
Takucap10 <t.okusako10@gmail.com>
2022-02-06 18:37:43 +0900

    新規メモ作成

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,6 +1,18 @@
 class MemosController < ApplicationController
 
   def index
+    @memos = Memo.order('created_at DESC')
+  end
+
+  def new
+    @memo = Memo.new
+  end
+
+  def create
+    @memo = Memo.new(memo_params)
+    if @memo.save
+      redirect_to root_path
+    else render :new
   end
 
 end
--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,6 +1,5 @@
 class Memo < ApplicationRecord
   validates :item, null: false
-  validates :info
 
   belongs_to :room
   belongs_to :user
