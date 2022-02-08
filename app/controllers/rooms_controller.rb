9097fd29af6e1091ff069a08baa284382a511740
Takucap10 <t.okusako10@gmail.com>
2022-02-08 21:03:31 +0900

    room編集機能　ビューの調整

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,8 @@
 class RoomsController < ApplicationController
-
+  before_action :set_item, only: [:edit, :update, :destroy]
   def index
   end
-  
+
   def new
     @room = Room.new
   end
@@ -16,8 +16,31 @@ class RoomsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @room.update(room_params)
+    redirect_to root_path
+    else
+    render :new
+    end
+  end
+
+  def destroy
+    if @room.destroy
+    redirect_to root_path
+    else
+    render :index
+    end
+  end
+
   private
   def room_params
     params.require(:room).permit(:name, user_ids:[])
+  end
+
+  def set_item
+    @room = Room.find(params[:id])
   end
 end
