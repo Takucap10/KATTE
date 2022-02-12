962fb58179ef3102e06bdc577d85bebd36f8acda
Takucap10 <t.okusako10@gmail.com>
2022-02-12 11:21:10 +0900

    room　編集削除機能改善
    編集：ルートに戻っていた部分を部屋に戻れるように。
    
    削除：メモがあった場合削除できない挙動を修正。

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
   validates :name, presence: true
 
-  has_many :memos
+  has_many :memos, dependent: :destroy
   has_many :room_users, dependent: :destroy
   has_many :users, through: :room_users, dependent: :destroy
 end
